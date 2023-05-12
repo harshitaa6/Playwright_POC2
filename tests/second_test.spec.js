c9db20c93db62533db682a6516b0c194ab6d9c36
Harshita <harshitajjadhav1611@gmail.com>
2023-05-12 16:44:05 +0530

    added second_test

--- tests/second_test.spec.js
+++ tests/second_test.spec.js
@@ -1,6 +1,22 @@
 const { test, expect } = require("@playwright/test");
 
-test("my first test", async ({ page }) => {
-  await page.goto("https://amazon.com");
-  await expect(page).toHaveTitle("amazon");
+test("my test", async ({ page }) => {
+  await page.goto("https://www.amazon.com");
+  await page.waitForSelector("#twotabsearchtextbox");
+  await page.fill("#twotabsearchtextbox", "samsung mobile");
+  await page.click("#nav-search-submit-button");
+  await page.waitForNavigation();
+  await page.click(".s-result-item:nth-child(4) h2 a");
+  await page.waitForNavigation();
 });
+
+// import { test, expect } from "@playwright/test";
+
+// test("test", async ({ page }) => {
+//   await page.goto("https://www.amazon.com/");
+//   await page.getByPlaceholder("Search Amazon").click();
+//   await page.getByPlaceholder("Search Amazon").fill("samsung mobile");
+//   await page.getByRole("button", { name: "Go" }).click();
+//   await page.click(".s-result-item:nth-child(4) h2 a");
+//     await page.locator('div:nth-child(9) > div > .s-widget-container > .s-card-container > div > div > div:nth-child(2) > div').click();
+// });
