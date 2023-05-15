867c283ff88205b7bb74d43d78b503b7ca7fbb90
Harshita <harshitajjadhav1611@gmail.com>
2023-05-15 13:25:43 +0530

    amazon test

--- tests/second_test.spec.js
+++ tests/second_test.spec.js
@@ -1,22 +1,10 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
 
-test("my test", async ({ page }) => {
-  await page.goto("https://www.amazon.com");
-  await page.waitForSelector("#twotabsearchtextbox");
-  await page.fill("#twotabsearchtextbox", "samsung mobile");
+test("test", async ({ page }) => {
+  await page.goto("https://www.amazon.com/");
+  await page.click("#twotabsearchtextbox");
+  await page.pause();
+  await page.locator("id=twotabsearchtextbox").fill("samsung mobile");
   await page.click("#nav-search-submit-button");
-  await page.waitForNavigation();
-  await page.click(".s-result-item:nth-child(4) h2 a");
-  await page.waitForNavigation();
+  await page.click(".s-result-item:nth-child(5) ");
 });
-
-// import { test, expect } from "@playwright/test";
-
-// test("test", async ({ page }) => {
-//   await page.goto("https://www.amazon.com/");
-//   await page.getByPlaceholder("Search Amazon").click();
-//   await page.getByPlaceholder("Search Amazon").fill("samsung mobile");
-//   await page.getByRole("button", { name: "Go" }).click();
-//   await page.click(".s-result-item:nth-child(4) h2 a");
-//     await page.locator('div:nth-child(9) > div > .s-widget-container > .s-card-container > div > div > div:nth-child(2) > div').click();
-// });
