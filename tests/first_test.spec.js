3b20a32696b77c06331133ea6e4dd5e876f07309
Harshita <harshitajjadhav1611@gmail.com>
2023-05-16 16:42:44 +0530

    amazon test 2

--- tests/first_test.spec.js
+++ tests/first_test.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from ("@playwright/test");
 
 test("my first test", async ({ page }) => {
   await page.goto("https://google.com");
--- tests/second_test.spec.js
+++ tests/second_test.spec.js
@@ -2,9 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://www.amazon.com/");
+  await page.waitForSelector("#twotabsearchtextbox");
   await page.click("#twotabsearchtextbox");
   await page.pause();
+  await page.waitForSelector("id=twotabsearchtextbox");
   await page.locator("id=twotabsearchtextbox").fill("samsung mobile");
+  await page.waitForSelector("#nav-search-submit-button");
   await page.click("#nav-search-submit-button");
-  await page.click(".s-result-item:nth-child(5) ");
+  await page.waitForSelector(".s-result-item:nth-child(5) h2 a ");
+  await page.click(".s-result-item:nth-child(5) h2 a ");
 });
